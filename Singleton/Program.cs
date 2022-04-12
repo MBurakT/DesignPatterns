4d091b5dd4cc5b39725b1817ba5d86fa43512f02
MBurakT <mbtbrk11@outlook.com>
2022-04-12 11:41:57 +0300

    BTK 36.4

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -10,6 +10,38 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
         }
+    }
+
+    internal class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        static object _lockObject = new object();
+
+        private CustomerManager()
+        {
+
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock (_lockObject)
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+            }
+
+            return _customerManager;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!!");
+        }
+
     }
 }
