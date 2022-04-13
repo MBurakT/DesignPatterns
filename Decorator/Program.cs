99c3a59364e3a9f3e153280fefc647448c5fdda6
MBurakT <mbtbrk11@outlook.com>
2022-04-13 11:05:47 +0300

    BTK 36.13

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -10,6 +10,65 @@ namespace Decorator
     {
         static void Main(string[] args)
         {
+            var personalCar = new PersonalCar { Make = "BMW", Model = "3.20", HirePrice = 2500 };
+
+            SpecialOffer specialOffer = new SpecialOffer(personalCar);
+            specialOffer.DiscountPercentenge = 10;
+
+            Console.WriteLine("Concrete: {0}", personalCar.HirePrice);
+            Console.WriteLine("Special offer: {0}", specialOffer.HirePrice);
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CarBase
+    {
+        public abstract string Make { get; set; }
+        public abstract string Model { get; set; }
+        public abstract decimal HirePrice { get; set; }
+    }
+
+    class PersonalCar : CarBase
+    {
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice { get; set; }
+    }
+
+    class CommercialCar : CarBase
+    {
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice { get; set; }
+    }
+
+    abstract class CarDecoratorBase : CarBase
+    {
+        CarBase _carBase;
+
+        public CarDecoratorBase(CarBase carBase)
+        {
+            _carBase = carBase;
+        }
+    }
+
+    class SpecialOffer : CarDecoratorBase
+    {
+        public int DiscountPercentenge { get; set; }
+        private readonly CarBase _carBase;
+
+        public SpecialOffer(CarBase carBase) : base(carBase)
+        {
+            _carBase = carBase;
+        }
+
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice
+        {
+            get { return _carBase.HirePrice - (_carBase.HirePrice * DiscountPercentenge / 100); }
+            set { }
         }
     }
 }
