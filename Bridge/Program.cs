d3b0b54d7a2310310e73e1295d912b5a82e4a5bb
MBurakT <mbtbrk11@outlook.com>
2022-04-13 11:30:46 +0300

    BTK 36.14

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -10,6 +10,53 @@ namespace Bridge
     {
         static void Main(string[] args)
         {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.messageSenderBase = new MailSender();
+            customerManager.UpdateCustomer();
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class MessageSenderBase
+    {
+        public void Save()
+        {
+            Console.WriteLine("Message Saved!");
+        }
+
+        public abstract void Send(Body body);
+    }
+
+    internal class Body
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+
+    class MailSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via MailSender", body.Text);
+        }
+    }
+
+    class SmsSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via SmsSender", body.Text);
+        }
+    }
+
+    class CustomerManager
+    {
+        public MessageSenderBase messageSenderBase { get; set; }
+        public void UpdateCustomer()
+        {
+            messageSenderBase.Send(new Body { Text = "Sometihng something" });
+            Console.WriteLine("Customer updated!");
         }
     }
 }
