a41e5c7962bb2e383d014f4bbebd8eac7c6e92be
MBurakT <mbtbrk11@outlook.com>
2022-04-14 13:49:20 +0300

    BTK 36.19

--- TemplateMethod/Program.cs
+++ TemplateMethod/Program.cs
@@ -10,7 +10,87 @@ namespace TemplateMethod
     {
         static void Main(string[] args)
         {
+            ScoringAlgorithm algorithm;
+            Console.WriteLine("Men");
+            algorithm = new MenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10, new TimeSpan(0,2,34)));
+            Console.WriteLine("Women");
+            algorithm = new WomenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10, new TimeSpan(0, 2, 34)));
+            Console.WriteLine("Children");
+            algorithm = new ChildrenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10, new TimeSpan(0, 2, 34)));
 
+            Console.ReadLine();
+        }
+    }
+
+    abstract class ScoringAlgorithm
+    {
+        public int GenerateScore(int hits, TimeSpan time)
+        {
+            int score = CalculateBaseScore(hits);
+            int reduction = CalculateReduction(time);
+            return CalculateOverallScore(score, reduction);
+        }
+
+        protected abstract int CalculateOverallScore(int score, int reduction);
+        public abstract int CalculateReduction(TimeSpan time);
+        public abstract int CalculateBaseScore(int hits);
+
+    }
+
+    class MenScoringAlgorithm : ScoringAlgorithm
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 100;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 5;
+        }
+
+        protected override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+    }
+
+    class WomenScoringAlgorithm : ScoringAlgorithm
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 100;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 3;
+        }
+
+        protected override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+    }
+
+    class ChildrenScoringAlgorithm : ScoringAlgorithm
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 80;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 2;
+        }
+
+        protected override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
         }
     }
 }
