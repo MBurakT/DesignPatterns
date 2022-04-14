bc9d1e7501eaab858f051e8785eadb2c162fcb27
MBurakT <mbtbrk11@outlook.com>
2022-04-14 14:09:44 +0300

    BTK 36.20

--- State/Program.cs
+++ State/Program.cs
@@ -10,6 +10,62 @@ namespace State
     {
         static void Main(string[] args)
         {
+            Context context = new Context();
+            ModifiedState modifiedState = new ModifiedState();
+            modifiedState.DoAction(context);
+            DeletedState deletedState = new DeletedState();
+            deletedState.DoAction(context);
+
+            Console.WriteLine(context.GetState());
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IState
+    {
+        void DoAction(Context context);
+    }
+
+    class ModifiedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State: Modified");
+            context.SetState(this);
+        }
+    }
+
+    class DeletedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Deleted");
+            context.SetState(this);
+        }
+    }
+
+    class AddedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Added");
+            context.SetState(this);
+        }
+    }
+
+    class Context
+    {
+        private IState _state;
+
+        public void SetState(IState state)
+        {
+            _state = state;
+        }
+
+        public IState GetState()
+        {
+            return _state;
         }
     }
 }
